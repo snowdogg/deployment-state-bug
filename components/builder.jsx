5ee250f58a6a02f07c661e47aefbdfbac378fb38
Sun Spoiler <andreascary@gmail.com>
2024-05-23 09:43:55 -0600

    updated data functionality

--- components/builder.jsx
+++ components/builder.jsx
@@ -8,14 +8,14 @@ import "../builder-registry";
 // Builder Public API Key set in .env file
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY);
 
-export function RenderBuilderContent({ content, model }) {
+export function RenderBuilderContent({ content, model, data }) {
   // Call the useIsPreviewing hook to determine if
   // the page is being previewed in Builder
   const isPreviewing = useIsPreviewing();
   // If "content" has a value or the page is being previewed in Builder,
   // render the BuilderComponent with the specified content and model props.
   if (content || isPreviewing) {
-    return <BuilderComponent content={content} model={model} />;
+    return <BuilderComponent content={content} model={model} data={data} />;
   }
   // If the "content" is falsy and the page is
   // not being previewed in Builder, render the
